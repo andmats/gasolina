e463984abdd7f64b50c85aa0b6a6aad6ef42359a
neptune <neptune@laptop.com>
2013-03-09 16:34:18 +0000

    Added code to calculate the graph

--- js/results.js
+++ js/results.js
@@ -1,17 +1,45 @@
 
+var interest = parseFloat($('#interest').val()) / 100.0 / 12.0;
+var rent = parseFloat($('#rent').val());
+var savings = parseFloat($('#savings').val());
+var price = parseFloat($('#price').val());
+var payments = parseFloat($('#payments').val());
 
+//alert (interest + " " + rent + " " + savings + " " + price + " " + payments);
+function costAtMonth(m)
+{
+    if ( payments - price * interest + savings * m * interest <= 0 )
+    {
+        return -1;
+    }
+    if ( payments < 0 || 1+interest < 0 )
+        return 0;
+        
+    return m*rent + payments*Math.log(payments/(payments - price * interest + savings * m * interest))
+                            /Math.log(1+interest)
+           - price + savings*m;
+}
+
+var test = [];
+for ( var i = 0; i <100*12; i+=120)
+{
+     test.push(costAtMonth(i));
+}
+//alert ( test.join("\n") );
 
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-	      ['Year', 'Sales'],
-    	  ['2004',  1463],
-    	  ['2005', 35],
-    	  ['2006', 110020],
-    	  ['2007',  1030]
-	]);        
+function drawChart() 
+{
+    graph = [['Month', 'Cost']];
+    
+    for ( var i = 0; i < 50*12; i+=120)
+    {
+        graph.push([i/12.0, costAtMonth(i)]);
+    }   
+     
+    var data = google.visualization.arrayToDataTable(graph);        
 	
     var options = {
         title: 'Expenses chart',
@@ -19,7 +47,7 @@ function drawChart() {
 	  	    title: "Total cost"
         },
         hAxis: {
-	  	    title: "Months from now"
+	  	    title: "Years from now"
         },
         legend: { position: "none"},
         curveType: "function"
