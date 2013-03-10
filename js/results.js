f31b66179c532597ee5a65925e03eaf15bd05ab2
neptune <neptune@laptop.com>
2013-03-10 02:46:31 +0000

    Added h2

--- js/results.js
+++ js/results.js
@@ -20,23 +20,62 @@ function costAtMonth(m)
            - price + savings*m;
 }
 
+function optimalN()
+{
+    return  parseInt(
+                (price * interest * (rent + savings) 
+                - (rent + savings)*Math.log(1+interest)
+                - payments*(rent+savings*interest))
+                / (savings*interest*(rent+savings))
+             );
+}
+
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
 
+function pluralOrNot(x, str)
+{
+    if ( x === 1 )
+        return x + " " + str;
+    return x + " " + str + "s";
+}
 function drawChart() 
 {
-    graph = [['Month', 'Cost']];
+    var data = new google.visualization.DataTable();
+    
+    data.addColumn('number', 'Month'); // Implicit domain label col.
+    data.addColumn('number', 'Cost'); // Implicit series 1 data col.
+    data.addColumn({type:'string', role:'annotation'}); // annotation role col.
+    data.addColumn({type:'string', role:'annotationText'});
+    
+    var optN = optimalN();
+    var haveAlreadyIncludedOptN = false;
+
+    if ( optN === NaN || optN < 0)
+    {
+        optN = 0;
+    }
     
     for ( var i = 0; i < 30*12; i+=1)
     {
+        if ( !haveAlreadyIncludedOptN && optN <= i)
+        {
+            data.addRow([optN/12.0, costAtMonth(optN), "Optimal", 
+                        "Optimal time to get the mortgage from now: " + pluralOrNot(Math.floor(optN/12), "year")
+                        + " and " + pluralOrNot(optN%12, "month")]);
+            haveAlreadyIncludedOptN = true;
+            
+            if ( i == optN )
+            {
+                continue;
+            }
+        }
         var cost = costAtMonth(i);
         if ( cost !== NaN )
         {
-            graph.push([i/12.0, cost]);
+            data.addRow([i/12.0, cost, null, null]);
         }   
     }
-     
-    var data = google.visualization.arrayToDataTable(graph);        
 	
     var options = {
         title: 'Expenses chart',
