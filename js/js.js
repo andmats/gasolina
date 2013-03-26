5e561e12698cc3acb4788280cd7fce337c8af6c9
neptune <neptune@neptune.com>
2013-03-26 13:26:50 +0000

    dded error checking in dialog

--- js/js.js
+++ js/js.js
@@ -11,33 +11,33 @@ $("#about").click(function(event) {
     });
 });
 
-function checkLength(o, n, min, max) 
-{
-    if (o.val().length > max || o.val().length < min) {
-        o.addClass("ui-state-error");
-        o.html("Length of " + n + " must be between " + min + " and " + max + ".");
-        return false;
-    } else {
-        return true;
-    }
-}
-
 function checkRegexp(o, regexp, n) 
 {
     if (!( regexp.test(o.val()) )) {
         o.addClass("ui-state-error");
-        o.html(n);
+        updateTips(n);
         return false;
     } else {
         return true;
     }
 }
 
-var $name = $("#name");
-var $email = $( "#email" );
-var $formContent = $("#form_content");
+var allFields = $( [] ).add( $( "#name" ) ).add( $( "#email" ) ).add( $( "#form_content" ) );
 
-var allFields = $( [] ).add( $name ).add( $email ).add( $formContent );
+function updateTips(msg)
+{
+    var $tips = $( ".validateTips" );
+    $tips.text(msg).addClass("ui-state-highlight");
+    setTimeout(function() {
+        $tips.removeClass("ui-state-highlight", 1500);
+    }, 500);
+}
+
+function clearDialogContactElems()
+{
+    $(".validateTips").html("");
+    allFields.val("").removeClass("ui-state-error");    
+}
 
 $("#dialog-contact").dialog({
     autoOpen : false,
@@ -45,22 +45,31 @@ $("#dialog-contact").dialog({
     modal : true,
     buttons : {
         "Submit" : function() {
-            var bValid = true;
+            var $email = $( "#email" );
+            var $formContent = $("#form_content");
             
-            bValid = bValid && checkLength(email, "email", 6, 80);
             // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-            bValid = bValid && checkRegexp(email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com");
-
+            var bValid = true;
+            bValid = bValid && checkRegexp($email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "Not valid email (eg. j.smith@abc.com)");
+      
+            if (bValid && $formContent.val().length === 0 ) {
+                updateTips("Please enter your comments.");
+                $formContent.addClass("ui-state-error");
+                bValid = false;
+            }
+            
             if (bValid) {
-                $(this).dialog("close");
+                clearDialogContactElems();
+                sendComments($("#name").val(), $email.val(), $formContent.val());               
             }
         },
         Cancel : function() {
+            clearDialogContactElems();
             $(this).dialog("close");
         }
     },
     close : function() {
-        allFields.val("").removeClass("ui-state-error");
+        clearDialogContactElems();
     }
 });
 
@@ -68,3 +77,8 @@ $("#report").click(function(event) {
     event.preventDefault();
     $("#dialog-contact").dialog("open");
 });
+
+function sendComments(name, email, comments)
+{
+    
+}
